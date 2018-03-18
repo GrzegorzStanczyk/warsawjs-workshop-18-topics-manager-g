39cbcca86337bb9a7fe95484ac42bcb7cb9eca1f
Future Front-End Developer <grzegorz.stanczyk@o2.pl>
2018-03-18 14:20:31 +0100

    Added dependencies and html skelethon

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import Helpers from './helpers'
-
+import Hellojs from 'hellojs'
+import Bulma from 'bulma'
 // const helpers = new Helpers
+
 console.log(`Topics Manager ${Helpers.test}`)
+console.log(`Topics Manager ${Hellojs}`)
